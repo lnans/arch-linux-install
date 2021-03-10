0347c6c353088ca402a0288dfd1f079761bb78a4
Nans Laupretre <nans.laupretre@gmail.com>
2021-03-10 18:21:18 +0100

    fix scripts path

--- user-assets.sh
+++ user-assets.sh
@@ -1,15 +1,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo '# Installing extensions ...'
-./${DIR}/extensions/install.sh
+.${DIR}/extensions/install.sh
 
 echo '# Installing fonts...'
-./${DIR}/fonts/install.sh
+.${DIR}/fonts/install.sh
 
 echo '# Installing wallpapers'
-./${DIR}/wallpapers/install.sh
+.${DIR}/wallpapers/install.sh
 
 echo '# Installing themes'
-./${DIR}/themes/Orchis-theme/install.sh -t all
-./${DIR}/themes/Tela-circle-icon-theme/install.sh -a
-./${DIR}/themes/Vimix-cursors/install.sh
+.${DIR}/themes/Orchis-theme/install.sh -t all
+.${DIR}/themes/Tela-circle-icon-theme/install.sh -a
+.${DIR}/themes/Vimix-cursors/install.sh
