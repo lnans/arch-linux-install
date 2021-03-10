a85d6459439a9b14c1dadacdffb4b49e81da676d
Nans Laupretre <nans.laupretre@gmail.com>
2021-03-10 17:38:54 +0100

    fix extensions install

--- extensions/install.sh
+++ extensions/install.sh
@@ -4,7 +4,7 @@ for file in $FILES
 do
   UUID=$(unzip -c $file metadata.json | grep uuid | cut -d \" -f4)
   mkdir -p ~/.local/share/gnome-shell/extensions/${UUID}
-  unzip -q ${DIR}/${file} -d ~/.local/share/gnome-shell/extensions/${UUID}/
+  unzip -q ${file} -d ~/.local/share/gnome-shell/extensions/${UUID}/
 
   gnome-shell-extension-tool -e $UUID
 done
