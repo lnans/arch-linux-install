fe1e80b627a1b5cb7f3dcbc5465dbf977f427d33
Nans Laupretre <nans.laupretre@gmail.com>
2021-03-10 19:16:12 +0100

    Add colors on script

--- user-assets.sh
+++ user-assets.sh
@@ -8,24 +8,24 @@ BGreen='\033[1;32m' # Color green
 NC='\033[0m' # No Color
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-echo -e '${BGreen}# Installing extensions ...${NC}'
+echo -e "${BGreen}# Installing extensions ...${NC}"
 bash ${DIR}/extensions/install.sh
 
-echo -e '${BGreen}# Installing fonts...${NC}'
+echo -e "${BGreen}# Installing fonts...${NC}"
 bash ${DIR}/fonts/install.sh
 
-echo -e '${BGreen}# Installing wallpapers${NC}'
+echo -e "${BGreen}# Installing wallpapers${NC}"
 bash ${DIR}/wallpaper/install.sh
 
-echo -e '${BGreen}# Installing themes${NC}'
+echo -e "${BGreen}# Installing themes${NC}"
 cd ${DIR}/themes/Orchis-theme
 ./install.sh -t all
 
-echo -e '${BGreen}# Installing icons${NC}'
+echo -e "${BGreen}# Installing icons${NC}"
 cd ${DIR}/themes/Tela-circle-icon-theme
 ./install.sh -a
 
-echo -e '${BGreen}# Installing cursors${NC}'
+echo -e "${BGreen}# Installing cursors${NC}"
 cd ${DIR}/themes/Vimix-cursors
 ./install.sh
 
